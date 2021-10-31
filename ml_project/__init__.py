4b5efefe8ed2e130d17a298da1b26c87fd4eca16
WK <62734698+WK-EE@users.noreply.github.com>
2021-10-30 23:16:33 -0700

    Add files via upload

---
+++ ml_project/__init__.py
@@ -0,0 +1,7 @@
+"""
+* @author: Wael Khalil
+"""
+
+# This file makes it easier for other scripts to
+# access the functions in other modules
+# we have not imports for now as everything is in the '__main__.py' file
